bade2fa9899cc710e20be84e18cabda3ad8eb2a7
liing0228 <74757651+liing0228@users.noreply.github.com>
2021-10-21 21:58:41 +0800

    Update hw1.c

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -3,11 +3,12 @@ int main(){
   int i, j, temp;
   int nums[9]={0,1,1,1,2,2,2,0,1,0};
 	for (i = numsSize-1; i >=0; --i){     
-        for (j = 0; j < i ; ++j)  
+        for (j = 0; j < i ; ++j){  
 			if (nums[j] > nums[j + 1])       {
 				temp = nums[j];
 				nums[j] = nums[j + 1];
 				nums[j + 1] = temp;
 			}
- 	}
+ 		}
+	}
 }
