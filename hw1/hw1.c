965622016d3bb26b9de431cf2eb63f0b2e14be77
liing0228 <74757651+liing0228@users.noreply.github.com>
2021-10-21 21:57:43 +0800

    Update hw1.c

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -1,10 +1,13 @@
-void sortColors(int* nums, int numsSize){
+#include<stdio.h>
+int main(){
   int i, j, temp;
-	for (i = numsSize-1; i >=0; --i)     
+  int nums[9]={0,1,1,1,2,2,2,0,1,0};
+	for (i = numsSize-1; i >=0; --i){     
         for (j = 0; j < i ; ++j)  
 			if (nums[j] > nums[j + 1])       {
 				temp = nums[j];
 				nums[j] = nums[j + 1];
 				nums[j + 1] = temp;
 			}
+ 	}
 }
